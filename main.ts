a9994f8b99ae882bd2ff2b6632c3c74c25d47e65
pguemed1211 <132060728+pguemed1211@users.noreply.github.com>
2023-05-05 12:17:28 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
-
+basic.showString("hola")
+for (let index = 0; index < 3; index++) {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.showIcon(IconNames.SmallHeart)
+}
+basic.clearScreen()
+basic.showString("Adiós")
